3844362f4764daadac50dfdd583b169e152a16c2
[Hari Prasad M] <[hariprasad_miriyam@homedepot.com]>
2017-11-14 19:48:56 +0530

    Updated jest unit test cases

--- src/user-form/component-user-form.js
+++ src/user-form/component-user-form.js
@@ -19,11 +19,11 @@ export const UserDataForm = ({ name, contact, dispatch }) => {
                 }} className='input-form'>
 			<div>
 				<label>User Name :</label>
-				<input type='text' id='userName' ref={(input) => { _name = input; }}/>
+				<input type='text' className='userName' ref={(input) => { _name = input; }}/>
 			</div>
 			<div>
 			<label>Contact :</label>
-			<input type='text' id='contact' ref={(input) => { _contact = input; }} />
+			<input type='text' className='contact' ref={(input) => { _contact = input; }} />
 			</div>
 			<button className="formSubmit">Submit</button>
 			</form></div>
--- src/user-form/component-user-form.test.js
+++ src/user-form/component-user-form.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 import renderer from 'react-test-renderer';
-import { UserDataForm } from './component-user-form';
+import {configure, shallow, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { MainApp } from '../main-app';
 
-it('should correctly render user form', () => {
-  const result = renderer.create(
-      <UserDataForm />
-  );
-  expect(result.toJSON()).toMatchSnapshot();
+configure({ adapter: new Adapter() });
+describe("", () => {
+    const initialState = [];
+    const mockStore = configureStore();
+    let store = mockStore(initialState);
+    it('should correctly render user form', () => {
+        const result = renderer.create(<Provider store={store}><MainApp /></Provider>);
+        expect(result.toJSON()).toMatchSnapshot();
+    });
+
+    it('should correctly display the added data to table', () => {
+        store = mockStore([{"name" : "Hari", "contact" : 123}]);
+        const wrapper = mount(<Provider store={store}><MainApp /></Provider>);
+        wrapper.find('.formSubmit').simulate('click');
+        expect(wrapper.find('.table-container').length).toEqual(1);
+    });
 });
